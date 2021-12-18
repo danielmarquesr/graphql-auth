da00a03ed250b3003ecad1e90c849d899e06ec6b
Daniel Marques <danielmarques7@hotmail.com>
2021-12-18 00:42:08 -0300

    Add SignUp, SignIn, Users, User and CurrentUser resolvers

--- src/graphql/modules/user/mutation.ts
+++ src/graphql/modules/user/mutation.ts
@@ -1,12 +1,13 @@
 import { PrismaClient, User } from '@prisma/client';
-import { hash } from 'bcrypt';
+import { hashSync, compareSync } from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
+import { sign } from 'jsonwebtoken';
 
-const { user } = new PrismaClient();
+const SALT_ROUNDS = 10;
 
 export default {
   Mutation: {
-    CreateUser: async (
+    SignUp: async (
       _parent: any,
       {
         input,
@@ -16,12 +17,39 @@ export default {
           email: User['email'];
           password: User['password'];
         };
-      }
+      },
+      { prisma }: { prisma: PrismaClient }
     ) => {
-      const passwordHashed = await hash(input.password, 10);
-      const data = { ...input, password: passwordHashed, id: uuidv4() };
-      const userCreated = await user.create({ data });
+      const password = hashSync(input.password, SALT_ROUNDS);
+      const data = { ...input, password, id: uuidv4() };
+      const userCreated = await prisma.user.create({ data });
       return userCreated;
+    },
+    SignIn: async (
+      _parent: any,
+      {
+        input: { email, password },
+      }: {
+        input: {
+          email: User['email'];
+          password: User['password'];
+        };
+      },
+      { prisma }: { prisma: PrismaClient }
+    ) => {
+      const userFound = await prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (!userFound || !compareSync(password, userFound.password))
+        throw new Error('Incorrect email or password');
+
+      const token = sign({ id: userFound.id }, process.env.SECRET!, {
+        algorithm: 'HS256',
+        expiresIn: '90d',
+      });
+
+      return { token };
     },
   },
 };
--- src/graphql/modules/user/query.ts
+++ src/graphql/modules/user/query.ts
@@ -1,19 +1,39 @@
-import { PrismaClient, User } from '@prisma/client';
-
-const { user } = new PrismaClient();
+import { User, PrismaClient } from '@prisma/client';
+import { Request } from 'express';
+import { validateAuth } from 'src/utils';
 
 export default {
   Query: {
-    Users: async () => {
-      const users = await user.findMany();
+    Users: async (
+      _source: any,
+      _args: any,
+      { req, prisma }: { req: Request; prisma: PrismaClient }
+    ) => {
+      validateAuth(req);
+
+      const users = await prisma.user.findMany();
       return users;
     },
-    User: async (_parent: any, { id }: { id: User['id'] }) => {
-      const userFound = await user.findUnique({ where: { id } });
+    User: async (
+      _source: any,
+      { id }: { id: User['id'] },
+      { req, prisma }: { req: Request; prisma: PrismaClient }
+    ) => {
+      validateAuth(req);
+
+      const userFound = await prisma.user.findUnique({ where: { id } });
 
       if (!userFound) throw new Error('User not found!');
 
       return userFound;
+    },
+    CurrentUser: async (
+      _source: any,
+      _args: any,
+      { req }: { req: Request }
+    ) => {
+      const currentUser = validateAuth(req);
+      return currentUser;
     },
   },
 };
