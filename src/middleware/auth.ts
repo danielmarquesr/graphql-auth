a84478d595b437a490b9187c0c650c42611844d8
Daniel Marques <danielmarques7@hotmail.com>
2022-01-15 20:17:44 -0300

    Create AuthError and add it on auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, User } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import { verify, JwtPayload } from 'jsonwebtoken';
+import { AuthError } from 'src/utils';
 
 interface Payload extends JwtPayload {
   id?: User['id'];
@@ -16,24 +17,24 @@ export const verifyTokenJWT = async (
     const { authorization } = req.headers;
 
     if (!authorization)
-      throw new Error('JWT token was not found in request headers');
+      throw new AuthError('JWT token was not found in request headers');
 
     const token = authorization.replace('Bearer ', '');
 
     const { SECRET } = process.env;
     const decoded = verify(token, SECRET || 'some-real-secret') as Payload;
 
-    if (!decoded?.id) throw new Error('Invalid token');
+    if (!decoded?.id) throw new AuthError('Invalid token');
 
     const userFound = await prisma.user.findUnique({
       where: { id: decoded.id },
     });
 
-    if (!userFound) throw new Error("User don't found");
+    if (!userFound) throw new AuthError("User don't found");
 
     req.user = userFound;
   } catch (error) {
-    req.error = error as Error;
+    req.error = error as AuthError;
   } finally {
     next();
   }
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,13 +1,21 @@
 import { Request } from 'express';
 
+export class AuthError extends Error {
+  constructor(...params: string[]) {
+    super(...params);
+    this.name = 'AuthError';
+    if (Error.captureStackTrace) Error.captureStackTrace(this, AuthError);
+  }
+}
+
 export const validateAuth = (req: Request) => {
   const { user, error } = req;
 
   if (error) throw error;
 
-  if (!user) throw new Error("Can't get the user from request");
+  if (!user) throw new AuthError("Can't get the user from request");
 
-  if (!user.isEmailConfirmed) throw new Error("User email isn't confirmed");
+  if (!user.isEmailConfirmed) throw new AuthError("User email isn't confirmed");
 
   return user;
 };
