6dcf2f9da2c2508a3784f1578aa5770f6dc256a6
Daniel Marques <danielmarques7@hotmail.com>
2022-03-09 00:33:14 -0300

    Mock jsonwebtoken and crypto package in setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,17 +3,32 @@ import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
 import prisma from '../prisma/client';
 
-jest.mock('../prisma/client', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}));
-
 jest.mock('uuid', () => ({
   v4: jest.fn().mockReturnValue('uuid-mock-test'),
 }));
 
 jest.mock('bcryptjs', () => ({
-  hashSync: jest.fn().mockReturnValue('hashed-mock-password'),
+  hashSync: jest.fn((password) => password),
+  compareSync: jest.fn((password, hashed) => password === hashed),
+}));
+
+jest.mock('jsonwebtoken', () => ({
+  ...jest.requireActual('jsonwebtoken'),
+  sign: jest.fn().mockReturnValue('jwt-token-mock-test'),
+}));
+
+jest.mock('../prisma/client', () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>(),
+}));
+
+jest.mock('crypto', () => ({
+  ...jest.requireActual('crypto'),
+  createHash: jest.fn().mockReturnValue({
+    update: jest.fn().mockReturnValue({
+      digest: jest.fn().mockReturnValue('hash-example'),
+    }),
+  }),
 }));
 
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
